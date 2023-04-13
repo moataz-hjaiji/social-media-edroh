2aa0ea6b75201917fef8ce686ecd36e07aff17df
moataz hjaiji <moatazhjaiji@takiacademyteam.com>
2023-04-13 15:33:51 +0100

    install packages && configuration app.js && storage file

---
+++ server/app.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import multer from 'multer';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+/*CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginOpenerPolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser({ limit: '30mb', extended: true }));
+appp.urlendcode({ limit: '30mb', extended: true });
+app.use(cors());
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/assets');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({storage})
